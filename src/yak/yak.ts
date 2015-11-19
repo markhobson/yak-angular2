c11830227f6d11ca32de71f6cca7716bf6759620
Mark Hobson <markhobson@gmail.com>
2015-11-19 15:59:57 +0000

    Displayed list of yaks

--- src/yak/yak.ts
+++ src/yak/yak.ts
@@ -4,10 +4,17 @@ import {bootstrap, Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/a
 	selector: 'yak',
 	template: `
 		<h1>Yak Shaving</h1>
+		<h2>Yaks</h2>
+		<ul>
+			<li *ng-for="#yak of yaks">
+				{{yak}}
+			</li>
+		</ul>
 		`,
 	directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
 class YakComponent {
+	public yaks = ["x", "y", "z"];
 }
 
 bootstrap(YakComponent);
